03aa59d06ee2b0de695e0144f9d3b723b5463056
Sekky61 <misa@majer.cz>
2025-04-13 15:38:39 +0200

    hyprlock: revisit module

--- modules/hyprland/hyprlock.nix
+++ modules/hyprland/hyprlock.nix
@@ -1,57 +1,20 @@
 {
   pkgs,
   username,
+  config,
   ...
 }: let
-  # TODO move to config option
-  myWallpaper = "~/.config/wallpapers/spyxfamily.png";
+  myWallpaper = builtins.toPath config.stylix.image;
 in {
   # Hyprlock is the lockscreen handling for hyprland
   # Hypridle manages automatic locking
 
-  environment.systemPackages = with pkgs; [
-    hyprlock # lock screen
-    hypridle # auto lock
-  ];
-
-  michal.programs.hyprland.keybinds = [
-    {
-      description = "Toggle drawing to screen"; # TODO toggle off does not work (kill it with super+q)
-      bind = {key = "F7";};
-      command = {
-        dispatcher = "togglespecialworkspace";
-        params = "gromit";
-      };
-    }
-    {
-      description = "Clear drawing";
-      bind = {
-        mods = ["SHIFT"];
-        key = "F7";
-      };
-      command = {params = "gromit-mpx --clear";};
-    }
-    {
-      description = "Drawing: Undo";
-      bind = {key = "F6";};
-      command = {params = "gromit-mpx --undo";};
-    }
-    {
-      description = "Drawing: Redo";
-      bind = {
-        mods = ["SHIFT"];
-        key = "F6";
-      };
-      command = {params = "gromit-mpx --redo";};
-    }
-  ];
-
-  home-manager.users.${username} = _: {
+  home-manager.users.${username} = {
     programs.hyprlock = {
       enable = true;
       settings = {
         general = {
-          grace = 10;
+          grace = 10; # Unlock without password up to x seconds after lock
           hide_cursor = true;
         };
         background = [
@@ -59,6 +22,10 @@ in {
             path = myWallpaper;
             blur_passes = 2;
             blur_size = 6;
+            contrast = 1;
+            brightness = 0.5;
+            vibrancy = 0.2;
+            vibrancy_darkness = 0.2;
           }
         ];
         input-field = [
@@ -107,23 +74,23 @@ in {
 
         listener = [
           {
-            timeout = 150; # 2.5min.
+            timeout = 180; # 3min.
             on-timeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
             on-resume = "brightnessctl -r"; # monitor backlight restore.
           }
           {
-            timeout = 155; # 2.5min.
+            timeout = 180; # 3min.
             on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
             on-resume = "brightnessctl -rd rgb:kbd_backlight"; # turn on keyboard backlight.
           }
           {
-            timeout = 900; # 15min
-            on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
-          }
-          {
-            timeout = 330; # 5.5min
+            timeout = 300; # 5min
             on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
             on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
+          }
+          {
+            timeout = 900; # 15min
+            on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
           }
           {
             timeout = 1800; # 30min
