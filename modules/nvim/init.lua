c8671b07f884daff10ddbe9c076e0e4c2604a4ba
Sekky61 <misa@majer.cz>
2025-04-13 13:13:44 +0200

    nvim: remove git commit shortcut

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -1877,12 +1877,6 @@ vim.keymap.set("n", "Q", "<Nop>")
 -- Git + Github
 vim.api.nvim_set_keymap(
     "n",
-    "<leader>gc",
-    ':Git commit -m "',
-    { noremap = false, desc = "Git Commit" }
-)
-vim.api.nvim_set_keymap(
-    "n",
     "<leader>gp",
     ":Git push -u origin HEAD<CR>",
     { noremap = false, desc = "Git Push" }
