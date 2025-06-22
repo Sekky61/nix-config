383149cb6b078fce06feb570aa97dfbba0da4750
Sekky61 <misa@majer.cz>
2025-06-22 23:30:42 +0200

    kdeconnect: add options

--- modules/gui-packages/kde-connect.nix
+++ modules/gui-packages/kde-connect.nix
@@ -1,12 +1,15 @@
-# An alternative called gsconnect is available, but should be about the same
 {
   username,
   config,
+  pkgs,
   lib,
   ...
 }:
 with lib; let
   cfg = config.michal.programs.kde-connect;
+  package = pkgs.kdePackages.kdeconnect-kde;
+  # or pkgs.gnomeExtensions.gsconnect;
+  # or pkgs.valent;
 in {
   options.michal.programs.kde-connect = {
     enable = mkEnableOption "kde-connect";
@@ -14,11 +17,15 @@ in {
 
   config = mkIf cfg.enable {
     # NixOS module just opens ports 1714-1764 tcp/udp
-    programs.kdeconnect.enable = true;
+    programs.kdeconnect = {
+      inherit package;
+      enable = true;
+    };
 
     # home-manager starts systemd service
     home-manager.users.${username} = {
       services.kdeconnect = {
+        inherit package;
         enable = true;
         indicator = true;
       };
