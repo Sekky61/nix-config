e996aafee9ba71721214124dd044bbb94da0239e
Sekky61 <misa@majer.cz>
2025-06-28 12:50:21 +0200

    refactor theme

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -1,14 +1,9 @@
 {
-  config,
-  pkgs,
   lib,
   inputs,
-  username,
   ...
 }:
 with lib; let
-  cfg = config.michal.theme;
-
   themeJson = builtins.fromJSON (readFile ./theme.json);
   # baseCss = config.lib.stylix.colors { # Handlebars formatting
   #   template = ./gtk_template.json;
@@ -30,13 +25,13 @@ in {
 
   imports = [
     inputs.stylix.nixosModules.stylix
+    ./graphical.nix
   ];
 
   config = {
     stylix = {
       enable = true;
       autoEnable = false; # conflicts with impure configs
-      image = ../../assets/wallpapers/spyxfamily.png;
       base16Scheme = with themeJson; {
         # Hyprland: background
         # Chrome: theme
@@ -64,49 +59,7 @@ in {
       opacity.terminal = 0.9;
       targets = {
         console.enable = false;
-        grub = {
-          enable = true;
-          useImage = true;
-        };
       };
-      fonts = {
-        serif = {
-          package = pkgs.roboto-serif;
-          name = "Roboto Serif";
-        };
-
-        sansSerif = {
-          package = pkgs.roboto;
-          name = "Roboto";
-        };
-
-        monospace = {
-          package = pkgs.nerd-fonts.monaspace;
-          name = "Monaspace Neon";
-        };
-
-        emoji = {
-          package = pkgs.noto-fonts-emoji;
-          name = "Noto Color Emoji";
-        };
-      };
-    };
-    home-manager.users.${username} = {
-      # Some of stylix only in Home-manager
-      stylix.targets = {
-        # Explicitly say what to style. Default is false with autoEnable = false
-        hyprland.enable = true;
-        hyprland.hyprpaper.enable = true;
-        hyprlock.enable = false;
-        alacritty.enable = false;
-        vscode.enable = false;
-        gtk.enable = false;
-      };
-
-      # xdg.configFile = {
-      #   "gtk-3.0/gtk.css".source = baseCss;
-      #   "gtk-4.0/gtk.css".source = baseCss;
-      # };
     };
 
     michal.theme = themeJson;
---
+++ modules/theme/graphical.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  username,
+  ...
+}:
+with lib; let
+  cfg = config.michal.graphical;
+in {
+  config = mkIf cfg.enable {
+    stylix = {
+      image = ../../assets/wallpapers/spyxfamily.png;
+      targets = {
+        grub = {
+          enable = true;
+          useImage = true;
+        };
+      };
+    };
+    home-manager.users.${username} = {
+      stylix.targets = {
+        hyprland.enable = true;
+        hyprland.hyprpaper.enable = true;
+        hyprlock.enable = false;
+        alacritty.enable = false;
+        vscode.enable = false;
+        gtk.enable = false;
+      };
+
+      gtk = {
+        enable = true;
+        font = {
+          name = "Roboto";
+          size = 12; # This size directs size of text in UI (bar, settings)
+        };
+        # theme.name = gtk-theme;
+
+        # Below is untested/unknown
+        cursorTheme = {
+          name = "Bibata-Modern-Classic";
+          package = pkgs.bibata-cursors;
+        };
+        iconTheme.name = "MoreWaita";
+        # gtk3.extraCss = ''
+        #   headerbar, .titlebar,
+        #   .csd:not(.popup):not(tooltip):not(messagedialog) decoration{
+        #     border-radius: 0;
+        #   }
+        # '';
+      };
+
+      qt = {
+        enable = true;
+        platformTheme.name = "kde";
+      };
+    };
+  };
+}
