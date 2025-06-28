a81bfdb6adc47d7029093e1961341b6e57db993b
Sekky61 <misa@majer.cz>
2025-06-28 12:40:11 +0200

    feat: Enable fuzzel.nix only if graphical.enable is true

--- modules/gui-packages/fuzzel.nix
+++ modules/gui-packages/fuzzel.nix
@@ -1,41 +1,45 @@
 {
   config,
   username,
+  lib,
   ...
 }: let
   defaultTerminal = config.michal.environment.terminal;
+  cfg = config.michal.graphical;
 in {
-  home-manager.users.${username} = {
-    programs.fuzzel = {
-      enable = true;
-      settings = {
-        main = {
-          terminal = "${defaultTerminal} -e";
-          prompt = ">>  ";
-          layer = "overlay";
-          match-counter = true;
-          lines = 16;
-          width = 50;
-        };
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${username} = {
+      programs.fuzzel = {
+        enable = true;
+        settings = {
+          main = {
+            terminal = "${defaultTerminal} -e";
+            prompt = ">>  ";
+            layer = "overlay";
+            match-counter = true;
+            lines = 16;
+            width = 50;
+          };
 
-        # Colors can be set by stylix but there is a HM issue
-        colors = {
-          background = "130F0Dff";
-          text = "ece0daff";
-          selection = "52443bff";
-          selection-text = "d7c3b8ff";
-          border = "52443bff";
-          match = "ffb783ff";
-          selection-match = "ffb783ff";
-        };
+          # Colors can be set by stylix but there is a HM issue
+          colors = {
+            background = "130F0Dff";
+            text = "ece0daff";
+            selection = "52443bff";
+            selection-text = "d7c3b8ff";
+            border = "52443bff";
+            match = "ffb783ff";
+            selection-match = "ffb783ff";
+          };
 
-        border = {
-          radius = 12;
-          width = 2;
-        };
+          border = {
+            radius = 12;
+            width = 2;
+          };
 
-        dmenu = {
-          exit-immediately-if-empty = true;
+          dmenu = {
+            exit-immediately-if-empty = true;
+          };
         };
       };
     };
