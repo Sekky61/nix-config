e5fdafe94918419acc880d7cf15c6725c31841b4
Sekky61 <misa@majer.cz>
2025-07-12 09:15:08 +0200

    opencode

--- modules/overlays/opencode-overlay.nix
+++ modules/overlays/opencode-overlay.nix
@@ -1,4 +1,4 @@
 final: prev: {
   # Use final by default, but callpackage is not a package so its ok
-  gemini-cli = prev.callPackage ../dev/packages/gemini-cli.nix {};
+  opencode = prev.callPackage ../dev/packages/opencode.nix {};
 }
