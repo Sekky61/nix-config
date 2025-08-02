b490adcdfa688e918f9f7d16fb8861093f0facaf
Sekky61 <misa@majer.cz>
2025-08-02 20:21:28 +0200

    cachix substituers

--- modules/cachix.nix
+++ modules/cachix.nix
@@ -3,7 +3,6 @@
   nix = {
     settings = {
       substituters = [
-        "https://hyprland.cachix.org"
         "https://nix-gaming.cachix.org"
         # Nixpkgs-Wayland
         "https://cache.nixos.org"
@@ -13,7 +12,6 @@
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         # Nixpkgs-Wayland
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
