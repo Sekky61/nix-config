94d035227728b4c61e687a8c288e92a399f19e30
Sekky61 <misa@majer.cz>
2025-08-05 22:49:27 +0200

    nvim: minuet tab completion with codestral

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -768,6 +768,53 @@ require("lazy").setup({
         },
     },
     {
+        -- inline AI autocomplete
+        "milanglacier/minuet-ai.nvim",
+        -- todo just use setup={} ?
+        config = function()
+            require("minuet").setup({
+                provider = "openai_compatible",
+                request_timeout = 2.5,
+                context_window = 12000,
+                throttle = 1500, -- Increase to reduce costs and avoid rate limits
+                debounce = 600, -- Increase to reduce costs and avoid rate limits
+                provider_options = {
+                    openai_compatible = {
+                        api_key = "OPENROUTER_API_KEY",
+                        end_point = "https://openrouter.ai/api/v1/chat/completions",
+                        model = "mistralai/codestral-2508",
+                        name = "Openrouter",
+                        optional = {
+                            max_tokens = 56,
+                            top_p = 0.9,
+                            provider = {
+                                -- Prioritize throughput for faster completion
+                                sort = "throughput",
+                            },
+                        },
+                    },
+                },
+                virtualtext = {
+                    auto_trigger_ft = {},
+                    keymap = {
+                        -- accept whole completion
+                        accept = "<A-A>",
+                        -- accept one line
+                        accept_line = "<A-a>",
+                        -- accept n lines (prompts for number)
+                        -- e.g. "A-z 2 CR" will accept 2 lines
+                        accept_n_lines = "<A-z>",
+                        -- Cycle to prev completion item, or manually invoke completion
+                        prev = "<A-[>",
+                        -- Cycle to next completion item, or manually invoke completion
+                        next = "<A-]>",
+                        dismiss = "<A-e>",
+                    },
+                },
+            })
+        end,
+    },
+    {
         "kdheepak/lazygit.nvim",
         lazy = true,
         cmd = {
