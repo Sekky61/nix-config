3d856214b98af72b70e60b57695676286f70627b
Sekky61 <misa@majer.cz>
2026-02-01 22:21:39 +0100

    feat: increase file watcher count

--- hosts/nix-fw/configuration.nix
+++ hosts/nix-fw/configuration.nix
@@ -31,7 +31,7 @@
 
   # Increase limits
   boot.kernel.sysctl = {
-    "fs.inotify.max_user_watches" = 1048576; # default: 8192
+    "fs.inotify.max_user_watches" = 1348576; # default: 8192
     "fs.inotify.max_user_instances" = 1024; # default: 128
     "fs.inotify.max_queued_events" = 32768; # default: 16384 };
   };
