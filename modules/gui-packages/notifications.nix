5a30c428aa725b427c5abdd1c1f277850d3045fd
Sekky61 <misa@majer.cz>
2026-02-05 22:15:15 +0100

    models: remove patch

--- modules/gui-packages/notifications.nix
+++ modules/gui-packages/notifications.nix
@@ -2,6 +2,16 @@
   home-manager.users.${username} = {
     services.mako = {
       enable = true;
+      settings = {
+        width = 300;
+        height = 100;
+        icons = true;
+        margin = "10";
+        padding = "8";
+        border-size = 3;
+        border-radius = 5;
+        default-timeout = 7000;
+      };
       extraConfig = ''
         # Colors
 
