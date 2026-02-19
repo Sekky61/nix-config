d0b2ac88bc5aae0b49bebaf5b62b716691457e2b
Sekky61 <misa@majer.cz>
2026-02-19 23:20:13 +0100

    nvim: move to michal unstable

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -3,14 +3,18 @@
   username,
   impurity,
   ...
-}: {
-  environment.systemPackages = with pkgs; [neovim]; # have it truly everywhere
+}: let
+  nvim = pkgs.michal-unstable.neovim-unwrapped;
+in {
+  # have it truly everywhere
+  environment.systemPackages = [nvim];
 
   home-manager.users.${username} = {
     xdg.configFile."nvim".source = impurity.link ./.; # TODO leave out the nix files
 
     programs.neovim = {
       enable = true;
+      package = nvim;
       defaultEditor = true;
     };
   };
