e7494ac3b4c51c591e3782a59ca2164b1f811ed2
Sekky61 <misa@majer.cz>
2026-03-24 16:10:03 +0100

    podman: podman-compose

--- modules/system/podman.nix
+++ modules/system/podman.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   username,
   ...
@@ -11,12 +12,18 @@ in {
 
   config = mkIf cfg.enable {
     # Podman is the default for running oci containers
+
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled =
         true; # Required for containers under podman-compose to be able to talk to each other.
     };
+
+    environment.systemPackages = with pkgs; [
+      podman-compose
+    ];
+
     # User permissions
     users.users.${username}.extraGroups = ["podman"];
   };
