91280b563047a8bed2f54dee2033e8940eb32e52
Sekky61 <misa@majer.cz>
2026-05-17 17:13:54 +0200

    hyprland: temporary linking fix

--- modules/hyprland/hyprland.lua
+++ modules/hyprland/hyprland.lua
@@ -1,5 +1,16 @@
 -- See https://wiki.hypr.land/Configuring/Start/
 
+local config_home = os.getenv("XDG_CONFIG_HOME")
+if config_home == nil or config_home == "" then
+  config_home = os.getenv("HOME") .. "/.config"
+end
+
+package.path = table.concat({
+  config_home .. "/hypr/?.lua",
+  config_home .. "/hypr/?/init.lua",
+  package.path,
+}, ";")
+
 require("hypr.settings")
 require("generated.monitors")
 require("hypr.rules")
