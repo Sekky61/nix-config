0e2fef4ef9c2a1e64d363963574644cce3c8b19e
Sekky61 <misa@majer.cz>
2026-05-20 17:14:19 +0200

    bluetooth: fix

--- modules/network/bluetooth.nix
+++ modules/network/bluetooth.nix
@@ -2,13 +2,21 @@
   config,
   pkgs,
   lib,
+  username,
   ...
 }:
 with lib; let
   graphical = config.michal.graphical.enable;
 in {
   config = mkIf config.hardware.bluetooth.enable {
-    services.blueman.enable = graphical;
+    services.blueman = {
+      enable = graphical;
+      withApplet = false;
+    };
+
+    home-manager.users.${username} = {
+      services.blueman-applet.enable = graphical;
+    };
 
     environment.systemPackages = with pkgs;
       [
