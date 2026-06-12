f36a196eecb9bab887808ea00cce097c83f0fed5
Sekky61 <misa@majer.cz>
2026-06-12 22:10:19 +0200

    notifications: formatting

--- modules/gui-packages/notifications.nix
+++ modules/gui-packages/notifications.nix
@@ -1,28 +1,36 @@
-{username, ...}: {
-  home-manager.users.${username} = {
-    services.mako = {
-      enable = true;
-      settings = {
-        width = 300;
-        height = 100;
-        icons = true;
-        margin = "10";
-        padding = "8";
-        border-size = 3;
-        border-radius = 5;
-        default-timeout = 7000;
+{
+  config,
+  lib,
+  username,
+  ...
+}: {
+  # DMS has its own notification config
+  config = lib.mkIf (!config.michal.programs.dms-shell.enable) {
+    home-manager.users.${username} = {
+      services.mako = {
+        enable = true;
+        settings = {
+          width = 300;
+          height = 100;
+          icons = true;
+          margin = "10";
+          padding = "8";
+          border-size = 3;
+          border-radius = 5;
+          default-timeout = 7000;
+        };
+        extraConfig = ''
+          # Colors
+
+          background-color=#303446
+          text-color=#c6d0f5
+          border-color=#a6d189
+          progress-color=over #414559
+
+          [urgency=high]
+          border-color=#ef9f76
+        '';
       };
-      extraConfig = ''
-        # Colors
-
-        background-color=#303446
-        text-color=#c6d0f5
-        border-color=#a6d189
-        progress-color=over #414559
-
-        [urgency=high]
-        border-color=#ef9f76
-      '';
     };
   };
 }
